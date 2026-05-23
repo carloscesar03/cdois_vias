503adb82717566c214ce9fbce3e6af109b3bfc1c
Carlos Cesar <carloscesarct03@gmail.com>
2026-05-23 11:41:16 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'fisc-ce388-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se encontrar, senão busca na rede
+        return response || fetch(event.request);
+      })
+  );
+});
